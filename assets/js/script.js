fc23aaf34cf67c7c4e04b29309fb69ecd886f553
John-Pena <jmp1495@gmail.com>
2022-03-15 20:21:41 -0500

    created var to pull from api website

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@
 //function for weather api that refreshes weekly
 
 //function for quote api that changes daily
+
+// curl -X GET "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote" -H "accept: text/plain"
+
+
+var pullQuote = function() {
+    console.log("function fires!");
+    
+    var quoteUrl = "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote";
+    console.log(quoteUrl);
+};
