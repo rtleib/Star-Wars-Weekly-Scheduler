e01e7fd5e026a0f543c03967ba08d6e3d19ade96
AllyGarcia <allygarcia152@gmail.com>
2022-03-21 21:53:54 -0500

    forgot to change branches but I got the daily schedule to change corresponding to the selected button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,237 @@
 // function for entering information on scheduler and saving to local storage
 // get the current date
 var date = document.getElementById("today");
-var milHours = ["9","10","11","12","13","14","15","16","17"];
 var saveBtnEl = document.querySelectorAll(".saveBtn");
 var currentHour = parseInt(moment().format('HH'));
+//function for weather api that refreshes weekly
+var quoteContainer = document.getElementById('random-quote');
+//create an array for each day
+var weeklySchedule = [
+  {
+    nineAm: "",
+    tenAm: "",
+    elevenAm: "",
+    twelvePm: "",
+    onePm: "",
+    twoPm: "",
+    threePm: "",
+    fourPm: "",
+    fivePm: "",
+  },
+  {
+    nineAm: "",
+    tenAm: "",
+    elevenAm: "",
+    twelvePm: "",
+    onePm: "",
+    twoPm: "",
+    threePm: "",
+    fourPm: "",
+    fivePm: "",
+  },
+  {
+    nineAm: "",
+    tenAm: "",
+    elevenAm: "",
+    twelvePm: "",
+    onePm: "",
+    twoPm: "",
+    threePm: "",
+    fourPm: "",
+    fivePm: "",
+  },
+  {
+    nineAm: "",
+    tenAm: "",
+    elevenAm: "",
+    twelvePm: "",
+    onePm: "",
+    twoPm: "",
+    threePm: "",
+    fourPm: "",
+    fivePm: "",
+  },
+  {
+    nineAm: "",
+    tenAm: "",
+    elevenAm: "",
+    twelvePm: "",
+    onePm: "",
+    twoPm: "",
+    threePm: "",
+    fourPm: "",
+    fivePm: "",
+  },
+  {
+    nineAm: "",
+    tenAm: "",
+    elevenAm: "",
+    twelvePm: "",
+    onePm: "",
+    twoPm: "",
+    threePm: "",
+    fourPm: "",
+    fivePm: "",
+  },
+  {
+    nineAm: "",
+    tenAm: "",
+    elevenAm: "",
+    twelvePm: "",
+    onePm: "",
+    twoPm: "",
+    threePm: "",
+    fourPm: "",
+    fivePm: "",
+  },
+];
+var day;
 
-// save tasks for each hour 
-var saveText = function(){
-  var hour = this.parentElement.parentElement.dataset.hour;
-  var value = this.parentElement.previousElementSibling.value;
-  console.log (hour);
-  console.log(value);
-  localStorage.setItem(hour, value);
+//load page
+if (localStorage.getItem("schedule") !== null) {
+  weeklySchedule = JSON.parse(localStorage.getItem("schedule"));
 };
 
-//reload tasks on reload
-var reload = function(){
-  milHours.forEach(function(hour){
-    //get items from localStorage
-    var timeBlock = document.querySelector(`[data-hour="${hour}"] textarea`);
-    timeBlock.value = localStorage.getItem(hour);
-    //set color coding by hour
-    if (parseInt(hour) > currentHour) {
-      timeBlock.classList.add("future")
-    } else if (parseInt(hour) === currentHour){
-      timeBlock.classList.add("present")
-    } else if (parseInt(hour) < currentHour) {
-      timeBlock.classList.add("past")
-    }
-    console.log(hour, typeof parseInt(hour));
-    console.log(currentHour, typeof currentHour);
-  });
-
+// save tasks for each hour 
+var saveText = function () {
+  var hour = this.parentElement.parentElement.dataset.hour;
+  var value = this.parentElement.previousElementSibling.value;
+  weeklySchedule[day][hour] = value
+  localStorage.setItem("schedule", JSON.stringify(weeklySchedule));
 };
 
 // date format apprearance 
 date.textContent = moment().format('MMMM Do, YYYY');
 
 // save items to schedule
-saveBtnEl.forEach(function(el) {
+saveBtnEl.forEach(function (el) {
   el.addEventListener("click", saveText);
 });
 
-reload();
+//change saveBtn icon on hover//
+saveBtnEl
 
+var pullQuote = function () {
+  var quoteUrl = "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote";
 
-//function for weather api that refreshes weekly
-
-var quoteContainer = document.getElementById('random-quote');
-
-var pullQuote = function() {
-    
-    var quoteUrl = "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote";
-
-    // pulls from api to get a random quote
-    fetch(quoteUrl).then(function(response) {
+  // pulls from api to get a random quote
+  fetch(quoteUrl).then(function (response) {
     console.log(response);
-        if (response.ok) {
-            response.json().then(function(data) {
-            console.log(data);
-            displayQuote(data);
-            });
-        };
-    });
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        displayQuote(data);
+      });
+    };
+  });
 };
 
-var displayQuote = function(quote) {
+var displayQuote = function (quote) {
+  // create a span to hold quotes
+  var quoteEl = document.createElement('span');
+  quoteEl.textContent = quote.content;
+  console.log(quoteEl);
 
-    // create a span to hold quotes
-    var quoteEl = document.createElement('span');
-    quoteEl.textContent = quote.content;
-    console.log(quoteEl);
-
-    // append to container
-    quoteContainer.appendChild(quoteEl);
+  // append to container
+  quoteContainer.appendChild(quoteEl);
 };
 
 pullQuote();
+
+document.getElementById("sunday").addEventListener("click", function () {
+  day = 0;
+  var textDescriptions = document.querySelectorAll(".description");
+  var position = 0;
+  var display = document.querySelector("#display");
+  display.classList.remove("hidden");
+  for (var key in weeklySchedule[day]) {
+    console.log(weeklySchedule[day][key]);
+    console.log(textDescriptions[position]);
+    textDescriptions[position].value = weeklySchedule[day][key];
+    position++;
+  }
+});
+
+document.getElementById("monday").addEventListener("click", function () {
+  day = 1;
+  var textDescriptions = document.querySelectorAll(".description");
+  var position = 0;
+  var display = document.querySelector("#display");
+  display.classList.remove("hidden");
+  for (var key in weeklySchedule[day]) {
+    console.log(weeklySchedule[day][key]);
+    console.log(textDescriptions[position]);
+    textDescriptions[position].value = weeklySchedule[day][key];
+    position++;
+  }
+});
+
+document.getElementById("tuesday").addEventListener("click", function () {
+  day = 2;
+  var textDescriptions = document.querySelectorAll(".description");
+  var position = 0;
+  var display = document.querySelector("#display");
+  display.classList.remove("hidden");
+  for (var key in weeklySchedule[day]) {
+    console.log(weeklySchedule[day][key]);
+    console.log(textDescriptions[position]);
+    textDescriptions[position].value = weeklySchedule[day][key];
+    position++;
+  }
+});
+
+document.getElementById("wednesday").addEventListener("click", function () {
+  day = 3;
+  var textDescriptions = document.querySelectorAll(".description");
+  var position = 0;
+  var display = document.querySelector("#display");
+  display.classList.remove("hidden");
+  for (var key in weeklySchedule[day]) {
+    console.log(weeklySchedule[day][key]);
+    console.log(textDescriptions[position]);
+    textDescriptions[position].value = weeklySchedule[day][key];
+    position++;
+  }
+});
+
+document.getElementById("thursday").addEventListener("click", function () {
+  day = 4;
+  var textDescriptions = document.querySelectorAll(".description");
+  var position = 0;
+  var display = document.querySelector("#display");
+  display.classList.remove("hidden");
+  for (var key in weeklySchedule[day]) {
+    console.log(weeklySchedule[day][key]);
+    console.log(textDescriptions[position]);
+    textDescriptions[position].value = weeklySchedule[day][key];
+    position++;
+  }
+});
+
+document.getElementById("friday").addEventListener("click", function () {
+  day = 5;
+  var textDescriptions = document.querySelectorAll(".description");
+  var position = 0;
+  var display = document.querySelector("#display");
+  display.classList.remove("hidden");
+  for (var key in weeklySchedule[day]) {
+    console.log(weeklySchedule[day][key]);
+    console.log(textDescriptions[position]);
+    textDescriptions[position].value = weeklySchedule[day][key];
+    position++;
+  }
+});
+
+document.getElementById("saturday").addEventListener("click", function () {
+  day = 6;
+  var textDescriptions = document.querySelectorAll(".description");
+  var position = 0;
+  var display = document.querySelector("#display");
+  display.classList.remove("hidden");
+  for (var key in weeklySchedule[day]) {
+    console.log(weeklySchedule[day][key]);
+    console.log(textDescriptions[position]);
+    textDescriptions[position].value = weeklySchedule[day][key];
+    position++;
+  }
+});
